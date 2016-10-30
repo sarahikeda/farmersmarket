4e0d33c436f52e53ed8622d65975eaef7649c59f
Sarah Ing <sarahikeda@gmail.com>
2016-10-30 13:28:16 -0500

    Add tests for value of each product

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,17 @@
-class Market
+require 'pry'
+class Checkout
+  ITEMS = {
+            CH1: '$3.11',
+            AP1: '$6.00',
+            CF1: '$11.23',
+            MK1: '$4.75'
+          }
 
+  def initialize
+
+  end
+
+  def scan(item)
+    ITEMS[item.to_sym]
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,33 +1,30 @@
 require 'rspec'
+require 'pry'
 require_relative '../lib/market'
 
-describe Market do
-  it 'should receive a list of product rules' do
+describe Checkout do
+  # it 'should receive a list of product rules' do
+  # end
 
-  end
-
-  context 'product prices' do
+  context 'scanning products' do
+    let(:checkout) { Checkout.new }
     it 'should return one chai with a value of $3.11' do
-
+      expect(checkout.scan('CH1')).to eq ('$3.11')
     end
 
     it 'should return one bag of apples with a value of $6.00' do
+       expect(checkout.scan('AP1')).to eq ('$6.00')
     end
 
      it 'should return one coffee with a value of $11.23' do
-
+       expect(checkout.scan('CF1')).to eq ('$11.23')
     end
 
     it 'should return one order of milk with a value of $4.75' do
-
+      expect(checkout.scan('MK1')).to eq ('$4.75')
     end
 
   end
 
-#   Basket: CH1, AP1, CF1, MK1
-# Total price expected: $20.34
-# Basket: MK1, AP1
-# Total price expected: $10.75
-# Basket: CF1, CF1
-# Total price expected: $11.23
+
 end
