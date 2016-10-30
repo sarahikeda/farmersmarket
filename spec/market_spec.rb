306fd4f0f18265f38784620d2cd99bd1597bb710
Sarah Ing <sarahikeda@gmail.com>
2016-10-30 13:11:17 -0500

    Add beginning tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,6 +1,33 @@
-require "rspec"
-require_relative "../lib/market"
+require 'rspec'
+require_relative '../lib/market'
 
 describe Market do
+  it 'should receive a list of product rules' do
 
+  end
+
+  context 'product prices' do
+    it 'should return one chai with a value of $3.11' do
+
+    end
+
+    it 'should return one bag of apples with a value of $6.00' do
+    end
+
+     it 'should return one coffee with a value of $11.23' do
+
+    end
+
+    it 'should return one order of milk with a value of $4.75' do
+
+    end
+
+  end
+
+#   Basket: CH1, AP1, CF1, MK1
+# Total price expected: $20.34
+# Basket: MK1, AP1
+# Total price expected: $10.75
+# Basket: CF1, CF1
+# Total price expected: $11.23
 end
